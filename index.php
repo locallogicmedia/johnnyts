851893564fa30c91504e55ee0892208daf72002d
locallogicmedia <kenny@locallogicmedia.com>
2011-02-17 23:28:54 -0500

    first upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('functions.php'); echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+	<head>
+		<title> Johnny T's </title>
+		</head>
+	<body>
+		Hello this is Kenny's SECOND app!
+	</body>
+</html>
